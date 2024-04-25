67b39cf44393933715fc9935c091de9acf76e898
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-04-25 08:27:28 +0530

    ♫♪♩·.¸¸.·♩♪♫ Ethix-MD ♫♪♩·.¸¸.·♩♪♫

---
+++ src/event/handler.js
@@ -0,0 +1,50 @@
+import { downloadMediaMessage, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import { serialize, decodeJid } from '../../lib/Serializer.js';
+import path from 'path';
+import fs from 'fs/promises';
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const Handler = async (chatUpdate, sock, logger) => {
+  try {
+    if (chatUpdate.type !== 'notify') return;
+
+    const m = serialize(JSON.parse(JSON.stringify(chatUpdate.messages[0])), sock, logger);
+    if (!m.message) return;
+
+    if (
+      m.key &&
+      m.key.remoteJid === 'status@broadcast' &&
+      process.env.AUTO_STATUS_SEEN === 'true'
+    ) {
+      await sock.readMessages([m.key]);
+    }
+
+    if (
+      m.type === 'protocolMessage' ||
+      m.type === 'senderKeyDistributionMessage' ||
+      !m.type ||
+      m.type === ''
+    ) {
+      return;
+    }
+
+    const { isGroup, type, sender, from, body } = m;
+    console.log(m);
+
+    const pluginFiles = await fs.readdir(path.join(__dirname, '..', 'plugin'));
+
+    for (const file of pluginFiles) {
+      if (file.endsWith('.js')) {
+        const pluginModule = await import(path.join(__dirname, '..', 'plugin', file));
+        const loadPlugins = pluginModule.default;
+        await loadPlugins(m, sock);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default Handler;
