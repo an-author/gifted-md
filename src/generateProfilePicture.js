10f990ccb1d364ee7e272cc71d0470715fdf1d24
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-20 23:17:09 +0530

    update src/generateProfilePicture.js

---
+++ src/generateProfilePicture.js
@@ -0,0 +1,14 @@
+import Jimp from 'jimp';
+
+const generateProfilePicture = async (buffer) => {
+  const jimp = await Jimp.read(buffer)
+	const min = jimp.getWidth()
+	const max = jimp.getHeight()
+	const cropped = jimp.crop(0, 0, min, max)
+	return {
+		img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+		preview: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG)
+	}
+};
+
+export default generateProfilePicture;
