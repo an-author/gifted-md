1909b5dc930f355265ce8ecd225bf7e1b5e36dce
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-21 10:36:00 +0530

    update src/uploader.js

--- src/uploader.js
+++ src/uploader.js
@@ -78,7 +78,7 @@ export const webp2mp4File = async (path) => {
       const result = 'https:' + $2('div#output > p.outfile > video > source').attr('src');
       resolve({
         status: true,
-        message: "Created By MRHRTZ",
+        message: "Created By Ethix-MD",
         result: result
       });
     } catch (err) {
