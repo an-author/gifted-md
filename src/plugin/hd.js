807f2fb8c1ef0e8a78dcdf596f014945ff13bdb7
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-22 09:44:01 +0530

    update src/plugin/hd.js

--- src/plugin/hd.js
+++ src/plugin/hd.js
@@ -13,18 +13,18 @@ const tourl = async (m, gss) => {
   const prefixMatch = m.body.match(/^[\\/!#.]/);
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const validCommands = ['tourl', 'hd'];
+  const validCommands = ['hdr', 'hd', 'remini', 'enhance', 'upscale'];
 
   if (validCommands.includes(cmd)) {
     if (!m.quoted || m.quoted.mtype !== 'imageMessage') {
-      return m.reply(`Send/Reply with an image to set your profile picture ${prefix + cmd}`);
+      return m.reply(`*Send/Reply with an Image to Enhance Your Picture Quality ${prefix + cmd}*`);
     }
     
     const media = await m.quoted.download();
 
     try {
         let proses = await remini(media, "enhance"); // Call remini directly
-        gss.sendMessage(m.from, { image: proses, caption: `enhanced by gss botwa` }, { quoted: m });
+        gss.sendMessage(m.from, { image: proses, caption: `> *Hey ${m.pushName} Here Is Your Enhanced Image By Ethix-MD*` }, { quoted: m });
       
     } catch (error) {
       console.error('Error processing media:', error);
