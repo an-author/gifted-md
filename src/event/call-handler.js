d7c62b0ce596c3cdd1a1a83b7244f7f76dca8311
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-23 11:07:53 +0530

    update src/event/call-handler.js

--- src/event/call-handler.js
+++ src/event/call-handler.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import config from '../config.cjs'
+
 const Callupdate = async (json, sock) => {
    for (const id of json) {
       if (id.status === "offer" && config.REJECT_CALL === "true") {
