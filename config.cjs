c1f340b19277ef62a114316ae1b6c5af3b045669
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-23 14:22:46 +0530

    update config.cjs

--- config.cjs
+++ config.cjs
@@ -2,16 +2,16 @@
 const fs = require("fs");
 require("dotenv").config();
 
-const toBool = (x) => x === "true";
-
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Your Session Id",
-  AUTO_STATUS_SEEN: toBool(process.env.AUTO_STATUS_SEEN) || true,
-  REJECT_CALL: process.env.REJECT_CALL || false,
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN === 'true',
+  REJECT_CALL: process.env.REJECT_CALL === 'false',
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "Sid bhai",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
-  WELCOME: process.env.WELCOME || false,
+  WELCOME: process.env.WELCOME === 'false'
 };
+
+console.log(config);
 
 module.exports = config;
