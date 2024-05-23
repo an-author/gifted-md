c3aa5235f5f0774d138d2752dac50e67a3213b07
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-23 18:49:13 +0530

    update src/event/call-handler.js

--- src/event/call-handler.js
+++ src/event/call-handler.js
@@ -2,7 +2,7 @@ import config from '../../config.cjs';
 
 const Callupdate = async (json, sock) => {
    for (const id of json) {
-      if (id.status === 'offer' && config.REJECT_CALL === 'true' ) {
+      if (id.status === 'offer' && config.REJECT_CALL ) {
          let msg = await sock.sendMessage(id.from, {
             text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
             mentions: [id.from],
