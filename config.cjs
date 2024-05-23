b3a8dae2745cee53c73ca85f285c37df52751857
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-23 18:48:40 +0530

    update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Your Session Id",
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN === 'true',
-  REJECT_CALL: process.env.REJECT_CALL === 'false',
-  MODE: process.env.MODE || "public",
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== 'true',
+  REJECT_CALL: process.env.REJECT_CALL !== 'false',
+  MODE: process.env.MODE || 'public',
   OWNER_NAME: process.env.OWNER_NAME || "Sid bhai",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
-  WELCOME: process.env.WELCOME === 'false'
+  WELCOME: process.env.WELCOME !== 'false',
 };
 
 console.log(config);
