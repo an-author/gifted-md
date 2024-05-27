a4a6e501b941f47e2470e3aa269014fbf3b67fad
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-27 09:20:05 +0530

    update src/plugin/play.js

--- src/plugin/play.js
+++ src/plugin/play.js
@@ -74,7 +74,7 @@ const play = async (m, Matrix) => {
       const publishDate = new Date(videoDetails.publishDate).toLocaleDateString();
       const viewCount = videoDetails.viewCount;
       const lengthSeconds = videoDetails.lengthSeconds;
-      const thumbnailUrl = videoDetails.[thumbnails.length - 1].url;
+      const thumbnailUrl = videoDetails.thumbnails[0].url;
 
       const msg = generateWAMessageFromContent(m.from, {
         viewOnceMessage: {
