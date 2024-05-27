c902e0a4f70835b66fdf0c8ffd69c00fa5e9f9cd
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-27 08:57:47 +0530

    update src/plugin/play.js

--- src/plugin/play.js
+++ src/plugin/play.js
@@ -1,9 +1,8 @@
 import ytdl from 'ytdl-core';
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
-import { Readable } from 'stream';
-
 const { generateWAMessageFromContent, proto } = pkg;
 
+// Global variable to store video quality options and index
 const videoMap = new Map();
 let videoIndex = 1;
 
@@ -30,10 +29,11 @@ const play = async (m, Matrix) => {
     if (!text) return m.reply('Please provide a YouTube link');
 
     try {
-      await Matrix.sendMessage(m.from, { text: " Searching for video..." }, { quoted: m });
+      await Matrix.sendMessage(m.from, { text: "🕘 Searching for video..." }, { quoted: m });
 
       const videoUrl = text;
       if (!ytdl.validateURL(videoUrl)) {
+        m.reply('Invalid YouTube URL');
         await Matrix.sendMessage(m.from, { text: "❌ Invalid YouTube URL" }, { quoted: m });
         return;
       }
@@ -43,27 +43,27 @@ const play = async (m, Matrix) => {
 
       if (videoFormats.length === 0) {
         m.reply('No video formats found.');
+        await Matrix.sendMessage(m.from, { text: "❌ No video formats found" }, { quoted: m });
         return;
       }
 
+
       const uniqueFormats = {};
       videoFormats.forEach(format => {
-        if (!uniqueFormats[format.qualityLabel]) {
-          uniqueFormats[format.qualityLabel] = format;
-        } else if (uniqueFormats[format.qualityLabel].container !== 'mp4' && format.container === 'mp4') {
+        if (!uniqueFormats[format.qualityLabel] || (uniqueFormats[format.qualityLabel].container !== 'mp4' && format.container === 'mp4')) {
           uniqueFormats[format.qualityLabel] = format;
         }
       });
 
       const qualityButtons = Object.values(uniqueFormats).map((format, index) => {
-        const uniqueId = `video_${videoIndex + index}`;
+        const uniqueId = `dlvideo_${videoIndex + index}`;
         videoMap.set(uniqueId, {
           url: videoUrl,
           format: format
         });
         return {
           title: `${format.qualityLabel} (${format.container})`,
-          description: `Size: ${(format.contentLength / (1024 * 1024)).toFixed(2)} MB`,
+          description: `Size: ${format.size}`,
           id: uniqueId
         };
       });
@@ -74,31 +74,29 @@ const play = async (m, Matrix) => {
       const publishDate = new Date(videoDetails.publishDate).toLocaleDateString();
       const viewCount = videoDetails.viewCount;
       const lengthSeconds = videoDetails.lengthSeconds;
-      const thumbnailUrl = videoDetails.thumbnails[0].url;
+      const thumbnailUrl = thumbnails[thumbnails.length - 1].url;
 
       const msg = generateWAMessageFromContent(m.from, {
         viewOnceMessage: {
           message: {
             interactiveMessage: proto.Message.InteractiveMessage.create({
               body: proto.Message.InteractiveMessage.Body.create({
-                text: `Ethix-MD Video Downloader\n\n🔍 *${title}*\n👤 Author: ${author}\n📅 Upload Date: ${publishDate}\n👁️ Views: ${viewCount}\n⏳ Duration: ${Math.floor(lengthSeconds / 60)}:${lengthSeconds % 60}\n\n🎵 Download audio or video with a single click.\n📌 Simply select a video from the list below to get started.\n\n`
+                text: `Ethix-MD YouTube Downloader\n\n🔍 *${title}*\n👤 Author: ${author}\n📅 Upload Date: ${publishDate}\n👁️ Views: ${viewCount}\n⏳ Duration: ${Math.floor(lengthSeconds / 60)}:${lengthSeconds % 60}\n\n🎵 Select the quality of the video you want to download.`
               }),
               footer: proto.Message.InteractiveMessage.Footer.create({
-                text: "*© Powered By Ethix-MD*"
+                text: "> © Powered By Ethix-MD"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
                 ...(await prepareWAMessageMedia({ image: { url: thumbnailUrl } }, { upload: Matrix.waUploadToServer })),
-                title: ``,
-                gifPlayback: true,
-                subtitle: "",
-                hasMediaAttachment: false 
+                title: videoInfo.videoDetails.title,
+                gifPlayback: true
               }),
               nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
                 buttons: [
                   {
                     name: "single_select",
                     buttonParamsJson: JSON.stringify({
-                      title: "📹 Select Video Quality",
+                      title: "🔖 Select Video Quality",
                       sections: [
                         {
                           title: "📽️ Available Qualities",
@@ -125,11 +123,14 @@ const play = async (m, Matrix) => {
       }, {});
 
       await Matrix.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id });
+      
       videoIndex += videoFormats.length;
     } catch (error) {
       console.error("Error processing your request:", error);
+      m.reply('Error processing your request.');
+      await Matrix.sendMessage(m.from, { text: "❌ Error processing your request" }, { quoted: m });
     }
-  } else if (selectedId) { // Handle selected video quality
+  } else if (selectedId) { 
     const selectedVideo = videoMap.get(selectedId);
 
     if (selectedVideo) {
@@ -151,7 +152,7 @@ const play = async (m, Matrix) => {
         const maxSizeBytes = maxSizeMB * 1024 * 1024;
 
         if (videoBuffer.length <= maxSizeBytes) {
-          // Send as video if size is within limit
+
           await Matrix.sendMessage(m.from, { video: videoBuffer, mimetype: 'video/mp4', caption: caption }, { quoted: m });
         } else {
           await Matrix.sendMessage(m.from, { document: videoBuffer, mimetype: 'video/mp4', fileName: `${title}.mp4`, caption: caption }, { quoted: m });
