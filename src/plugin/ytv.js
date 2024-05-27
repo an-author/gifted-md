634069772c6b0fc40f5b9e2531471ecdbbf4f728
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-27 14:00:08 +0530

    update src/plugin/ytv.js

--- src/plugin/ytv.js
+++ src/plugin/ytv.js
@@ -168,7 +168,7 @@ const formatDuration = (seconds) => {
 
 const formatSize = (size) => {
   if (size < 1024) return `${size.toFixed(2)} B`;
-  if (size < 1024 * KB) return `${(size / 1024).toFixed(2)} KB`;
+  if (size < 1024 * ) return `${(size / 1024).toFixed(2)} KB`;
   if (size < 1024 * 1024) return `${(size / 1024 / 1024).toFixed(2)} MB`;
   return `${(size / 1024 / 1024 / 1024).toFixed(2)} GB`;
 };
