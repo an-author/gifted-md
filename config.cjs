497bd93e6d8919ead82b0127bfff0f39eef7581e
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-28 23:36:40 +0530

    update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ require("dotenv").config();
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Your Session Id",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
-  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'false' : true,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'false' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'false' : false,
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'false' : false,
   ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'false' : false,
@@ -16,12 +16,11 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   ANTI_VIEW_ONCE: process.env.ANTI_VIEW_ONCE !==
   undefined ? process.env.ANTI_VIEW_ONCE === 'true': true,
-  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : false,
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'public',
   OWNER_NAME: process.env.OWNER_NAME || "Sid bhai",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
-  ANTIBOT: process.env.ANTIBOT !== undefined ? process.env.ANTIBOT === "true" : true,
 };
 
 
