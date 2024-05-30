7af2d06d2de4c753d106ac6fac8f6afb712a566a
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-30 21:17:10 +0530

    update src/event/handler.js

--- src/event/handler.js
+++ src/event/handler.js
@@ -11,6 +11,16 @@ const userCommandCounts = new Map();
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 
+const getMessage = async (key, store) => {
+    if (store) {
+        const msg = await store.loadMessage(key.remoteJid, key.id);
+        return msg.message || undefined;
+    }
+    return {
+        conversation: "Hai Im gss botwa"
+    };
+};
+
 // Function to get group admins
 export const getGroupAdmins = (participants) => {
     let admins = [];
@@ -22,23 +32,51 @@ export const getGroupAdmins = (participants) => {
     return admins || [];
 };
 
+const deleteUpdate = async (message, sock, store) => {
+    try {
+        const { fromMe, id, participant } = message;
+        if (fromMe || !participant) return; // Check if participant is defined
 
+        const msg = await getMessage({ remoteJid: message.key.remoteJid, id: message.key.id }, store); // Fetch the original message
+        if (!msg) return;
 
-const Handler = async (chatUpdate, sock, logger) => {
+        const serializedMsg = serialize(msg, sock); // Serialize the fetched message
+        if (!serializedMsg) return;
+
+        await sock.sendMessage(serializedMsg.from, {
+            text: `
+≡ deleted a message 
+┌─⊷ 𝘼𝙉𝙏𝙄 𝘿𝙀𝙇𝙀𝙏𝙀 
+▢ *Number :* @${participant.split`@`[0]} 
+└─────────────
+            `.trim(),
+            mentions: [participant]
+        }, { quoted: serializedMsg });
+
+        await sock.copyNForward(serializedMsg.from, serializedMsg, false).catch(e => console.log(e, serializedMsg));
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const Handler = async (chatUpdate, sock, logger, store) => {
     try {
         if (chatUpdate.type !== 'notify') return;
 
         const m = serialize(JSON.parse(JSON.stringify(chatUpdate.messages[0])), sock, logger);
         if (!m.message) return;
-        
-        const botNumber = await sock.user.id.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-        const onrNumber = m.from.match(/(\d+)/);
-        const isCreator = [botNumber, onrNumber !== config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
 
         const participants = m.isGroup ? await sock.groupMetadata(m.from).then(metadata => metadata.participants) : [];
         const groupAdmins = m.isGroup ? getGroupAdmins(participants) : [];
-        const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false;
+        const isBotAdmins = m.isGroup ? groupAdmins.includes(m.isSelf) : false;
         const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false;
+
+        const PREFIX = /^[\\/!#.]/;
+        const isCOMMAND = (body) => PREFIX.test(body);
+        const prefixMatch = isCOMMAND(m.body) ? m.body.match(PREFIX) : null;
+        const prefix = prefixMatch ? prefixMatch[0] : '/';
+        const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+        const text = m.body.slice(prefix.length + cmd.length).trim();
 
         if (
             m.key &&
@@ -48,20 +86,30 @@ const Handler = async (chatUpdate, sock, logger) => {
             await sock.readMessages([m.key]);
         }
 
-        
+        const onrNumber = m.from.match(/\d+/)[0];
+        const botNumber = await sock.user.id.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+        const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
 
         if (!sock.public) {
             if (!isCreator) {
                 return;
             }
         }
-/*
+/* // ANTIBOT TEMPERERY OFF //
+        if (m.isGroup && m.key && m.key.id.startsWith("BAE5") && m.key.id.length === 16) {
+            if (!isBotAdmins) return m.reply(`Eh, the bot is not an admin`);
+            if (isAdmins) return m.reply(`Ehh, sorry you are admin`);
+            await sock.sendMessage(m.from, { text: "Bot detect" }, { quoted: m });
+            await sock.groupParticipantsUpdate(m.from, [m.sender], 'remove');
+            return;
+        }
+*/
         const groupChatId = '120363162694704836@g.us';
         const groupLink = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp';
         const commandLimit = 10; // Daily command limit
         const oneDayInMs = 24 * 60 * 60 * 1000; // Milliseconds in a day
 
-        if (!m.isSelf && onrNumber !==config.OWNER_NUMBER && config.NOT_ALLOW) {
+        if (isCOMMAND(m.body) && config.NOT_ALLOW) {
             const groupMetadata = await sock.groupMetadata(groupChatId);
             const participants = groupMetadata.participants;
             const participantIndex = participants.map(participant => participant.id);
@@ -99,19 +147,12 @@ const Handler = async (chatUpdate, sock, logger) => {
                 }
             }
         }
-        */
 
-        if (
-            m.type === 'protocolMessage' ||
-            m.type === 'senderKeyDistributionMessage' ||
-            !m.type ||
-            m.type === ''
-        ) {
-            return;
-        }
+        // Call deleteUpdate for all messages
+        await deleteUpdate(m, sock, store);
 
         const { isGroup, type, sender, from, body } = m;
-      //  console.log(m);
+        console.log(m);
 
         const pluginFiles = await fs.readdir(path.join(__dirname, '..', 'plugin'));
 
