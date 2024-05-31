c4e73867aace5247e3fc532c39b6611f6b8873c9
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-31 23:12:10 +0530

    update src/plugin/autotyping.js

---
+++ src/plugin/autotyping.js
@@ -0,0 +1,31 @@
+import config from '../../config.cjs';
+
+const autotypingCommand = async (m, Matrix) => {
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
+
+  if (cmd === 'autotyping') {
+    let responseMessage;
+
+    if (text === 'on') {
+      config.AUTO_TYPING = true;
+      responseMessage = "Auto-Typing has been enabled.";
+    } else if (text === 'off') {
+      config.AUTO_TYPING = false;
+      responseMessage = "Auto-Typing has been disabled.";
+    } else {
+      responseMessage = "Usage:\n- `autotyping on`: Enable Auto-Typing\n- `autotyping off`: Disable Auto-Typing";
+    }
+
+    try {
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
+    }
+  }
+};
+
+export default autotypingCommand;
