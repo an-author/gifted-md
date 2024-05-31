61b09afd4877e9459ee4f3220711db308c758fc9
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-05-31 23:10:38 +0530

    update src/plugin/alwaysonline.js

---
+++ src/plugin/alwaysonline.js
@@ -0,0 +1,31 @@
+import config from '../../config.cjs';
+
+const alwaysonlineCommand = async (m, Matrix) => {
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
+
+  if (cmd === 'alwaysonline') {
+    let responseMessage;
+
+    if (text === 'on') {
+      config.ALWAYS_ONLINE = true;
+      responseMessage = "Always Online has been enabled.";
+    } else if (text === 'off') {
+      config.ALWAYS_ONLINE = false;
+      responseMessage = "Always Online has been disabled.";
+    } else {
+      responseMessage = "Usage:\n- `alwaysonline on`: Enable Always Online\n- `alwaysonline off`: Disable Always Online";
+    }
+
+    try {
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
+    }
+  }
+};
+
+export default alwaysonlineCommand;
