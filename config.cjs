962c911fb7c7089933af2f078ff932d5fe768c22
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-01 18:17:00 +0530

    update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,8 +18,6 @@ const config = {
   MODE: process.env.MODE || 'public',
   OWNER_NAME: process.env.OWNER_NAME || "Sid bhai",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
-  PACKNAME: process.env.PACKNAME || "(¯´•._.• 𝐄𝐭𝐡𝐢𝐱-𝐌𝐃 •._.•´¯)",
-  AUTHOR: process.env.AUTHOR || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
