195983d277287f8c4e20f875fbfa2412d490a6e4
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-03 16:16:41 +0530

    update src/plugin/welcome.js

--- src/plugin/welcome.js
+++ src/plugin/welcome.js
@@ -1,7 +1,7 @@
 import config from '../../config.cjs';
 
 const autoblockCommand = async (m, Matrix) => {
-  const botNumber = await gss.decodeJid(gss.user.id);
+  const botNumber = await Matrix.decodeJid(Matrix.user.id);
   const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
   const prefixMatch = m.body.match(/^[\\/!#.]/);
   const prefix = prefixMatch ? prefixMatch[0] : '/';
