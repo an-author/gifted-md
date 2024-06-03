1dd7c619acf1e29e5e0b24d9cfc4921009f8a05f
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-03 16:46:55 +0530

    update src/plugin/del.js

--- src/plugin/del.js
+++ src/plugin/del.js
@@ -1,6 +1,12 @@
 import config from '../../config.cjs'
 
 const deleteMessage = async (m, gss) => {
+  
+  const fatkuns = (m.quoted || m)
+        const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
+        const mime = (quoted.msg || quoted).mimetype || ''
+        const qmsg = (quoted.msg || quoted)
+        const isMedia = /image|video|sticker|audio/.test(mime)
   const botNumber = await gss.decodeJid(gss.user.id);
   const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
   const prefixMatch = m.body.match(/^[\\/!#.]/);
@@ -11,7 +17,7 @@ const deleteMessage = async (m, gss) => {
 
    if (validCommands.includes(cmd)) {
      if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
-            if (!m.quoted || m.quoted.mtype !== 'conversation') return m.reply('Pʟᴇᴀsᴇ ᴍᴇɴᴛɪᴏɴ ᴀ ᴍᴇssᴀɢᴇ');
+        if (!m.quoted) return m.reply('Pʟᴇᴀsᴇ ᴍᴇɴᴛɪᴏɴ ᴀ ᴍᴇssᴀɢᴇ');
         let { from, id } = m.quoted;
 
         const key = {
