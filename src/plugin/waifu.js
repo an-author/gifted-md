d38ef5d7745f1dd06b1509b275e6fa180397b2c2
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-04 11:29:26 +0530

    update src/plugin/waifu.js

--- src/plugin/waifu.js
+++ src/plugin/waifu.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
-const stickerCommandHandler = async (m, gss) => {
+
+const stickerCommandHandler = async (m) => {
   const prefixMatch = m.body.match(/^[\\/!#.]/);
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   
-const stickerCommands = ['cry', 'kiss', 'kill', 'kick'];
+  const stickerCommands = ['cry', 'kiss', 'kill', 'kick'];
 
   if (stickerCommands.includes(cmd)) {
+    const mentionedUsers = m.mentionedIds.map(id => '@' + id.replace('@c.us', ''));
+    const senderName = m.sender ? '@' + m.sender.replace('@c.us', '') : '';
+    const packname = `${global.packname} ${mentionedUsers.join(' ')}`;
+    const author = `${global.author} ${senderName}`;
+
     try {
       const { data } = await axios.get(`https://api.waifu.pics/sfw/${cmd}`);
       if (data && data.url) {
-        gss.sendImageAsSticker(m.from, data.url, m, { packname: "", author: "" });
+        gss.sendImageAsSticker(m.from, data.url, m, { packname, author });
       } else {
         m.reply('Error fetching sticker.');
       }
