3742b142ce0f6e2dd6b12b5316ba32be4ff97352
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-04 11:21:14 +0530

    update src/plugin/waifu.js

--- src/plugin/waifu.js
+++ src/plugin/waifu.js
@@ -8,18 +8,10 @@ const stickerCommandHandler = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   
   if (stickerCommands.includes(cmd)) {
-    const mentionedUsers = m.mentionedIds.map(id => '@' + id.replace('@c.us', ''));
-    const senderName = m.sender ? '@' + m.sender.replace('@c.us', '') : '';
-    let message = `${senderName} ${cmd}`;
-    if (mentionedUsers.length > 0) {
-      message += ` ${mentionedUsers.join(' ')}`;
-    }
-
     try {
       const { data } = await axios.get(`https://api.waifu.pics/sfw/${cmd}`);
       if (data && data.url) {
-        gss.sendImageAsSticker(m.from, data.url, { quoted: m, caption: '' }, { packname: "", author: "" });
-        gss.sendMessage(m.from, message, { quoted: m });
+        gss.sendImageAsSticker(m.chat, data.url, m, { packname: "", author: "" });
       } else {
         m.reply('Error fetching sticker.');
       }
