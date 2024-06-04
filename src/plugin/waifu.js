c624efabc37d93ec0e8c0a9dc62ce475a0833ee9
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-04 11:13:34 +0530

    update src/plugin/waifu.js

--- src/plugin/waifu.js
+++ src/plugin/waifu.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const stickerCommands = ['cry', 'kiss', 'kill', 'kick'];
 
-const stickerCommandHandler = async (m) => {
+const stickerCommandHandler = async (m, gss) => {
   const prefixMatch = m.body.match(/^[\\/!#.]/);
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
