2a8257d04edced49918aca833b0792bf22903f86
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-04 07:18:20 +0530

    update src/plugin/tempmail.js

--- src/plugin/tempmail.js
+++ src/plugin/tempmail.js
@@ -1,4 +1,3 @@
-import pkg from '@whiskeysockets/baileys';
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
 import fetch from 'node-fetch'; // Import fetch for Node.js environment
