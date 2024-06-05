971e69146d53b8aa0b7ee350df01e1416f51a965
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-05 21:37:40 +0530

    update src/plugin/unblock.js

---
+++ src/plugin/unblock.js
@@ -0,0 +1,24 @@
+const unblock = async (m, gss) => {
+  try {
+    const botNumber = await gss.decodeJid(gss.user.id);
+    const prefixMatch = m.body.match(/^[\\/!#.]/);
+    const prefix = prefixMatch ? prefixMatch[0] : '/';
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    const text = m.body.slice(prefix.length + cmd.length).trim();
+
+    const validCommands = ['unblock'];
+
+    if (!validCommands.includes(cmd)) return;
+
+    let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+    
+    await gss.updateBlockStatus(users, 'unblock')
+      .then((res) => m.reply(`Unblocked ${users.split('@')[0]} successfully.`))
+      .catch((err) => m.reply(`Failed to unblock user: ${err}`));
+  } catch (error) {
+    console.error('Error:', error);
+    m.reply('An error occurred while processing the command.');
+  }
+};
+
+export default unblock;
