8ff9d7e3b5ebd9e79e0ffe679c887850a361dd75
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-05 16:58:09 +0530

    update src/plugin/promote.js

--- src/plugin/promote.js
+++ src/plugin/promote.js
@@ -10,14 +10,14 @@ const promote = async (m, gss) => {
 
     if (!validCommands.includes(cmd)) return;
 
-    if (!m.isGroup) return m.reply("*🚫 THIS COMMAND CAN ONLY BE USED IN GROUPS.");
+    if (!m.isGroup) return m.reply("*🚫 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*🚫 BOT MUST BE AN ADMIN TO USE THIS COMMAND.");
-    if (!senderAdmin) return m.reply("*🚫 YOU MUST BE AN ADMIN TO USE THIS COMMAND.");
+    if (!botAdmin) return m.reply("*🚫 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!senderAdmin) return m.reply("*🚫 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
 
     if (!m.mentionedJid) m.mentionedJid = [];
 
