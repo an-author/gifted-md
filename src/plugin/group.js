d13ae0aa684abbd846da1a88a343c34d73780698
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-06 13:14:46 +0530

    update src/plugin/group.js

--- src/plugin/group.js
+++ src/plugin/group.js
@@ -1,4 +1,4 @@
-const groupSetting = async (m, gss) => {
+const groupsetting = async (m, gss) => {
   try {
     const prefixMatch = m.body.match(/^[\\/!#.]/);
     const prefix = prefixMatch ? prefixMatch[0] : '/';
@@ -39,4 +39,4 @@ const groupSetting = async (m, gss) => {
   }
 };
 
-export default groupSetting;
+export default groupsetting;
