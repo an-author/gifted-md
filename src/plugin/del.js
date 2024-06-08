28b8d31feb28d1b257d61a0bf8ccad45e18ea1bd
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-06-08 10:00:29 +0530

    update src/plugin/del.js

--- src/plugin/del.js
+++ src/plugin/del.js
@@ -1,32 +1,35 @@
-import config from '../../config.cjs'
+import config from '../../config.cjs';
 
 const deleteMessage = async (m, gss) => {
-  
-  const fatkuns = (m.quoted || m)
-        const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
-        const mime = (quoted.msg || quoted).mimetype || ''
-        const qmsg = (quoted.msg || quoted)
-        const isMedia = /image|video|sticker|audio/.test(mime)
-  const botNumber = await gss.decodeJid(gss.user.id);
-  const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
-  const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  try {
+    const botNumber = await gss.decodeJid(gss.user.id);
+    const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
+    const prefixMatch = m.body.match(/^[\\/!#.]/);
+    const prefix = prefixMatch ? prefixMatch[0] : '/';
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
-  const validCommands = ['del', 'delete'];
+    const validCommands = ['del', 'delete'];
 
-   if (validCommands.includes(cmd)) {
-     if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
-        if (!m.quoted) return m.reply('Pʟᴇᴀsᴇ ᴍᴇɴᴛɪᴏɴ ᴀ ᴍᴇssᴀɢᴇ');
-        let { from, id } = m.quoted;
+    if (validCommands.includes(cmd)) {
+      if (!isCreator) {
+        return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+      }
 
-        const key = {
-            remoteJid: m.from,
-            id: m.quoted.id,
-            participant: m.quoted.sender
-        };
+      if (!m.quoted) {
+        return m.reply('✳️ Reply to the message you want to delete');
+      }
 
-        await gss.sendMessage(m.from, { delete: key });
+      const key = {
+        remoteJid: m.from,
+        id: m.quoted.key.id,
+        participant: m.quoted.key.participant || m.quoted.key.remoteJid
+      };
+
+      await gss.sendMessage(m.from, { delete: key });
+    }
+  } catch (error) {
+    console.error('Error deleting message:', error);
+    m.reply('An error occurred while trying to delete the message.');
   }
 };
 
