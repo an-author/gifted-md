4a2d88db136a6cb8df42e4e853767e230476f835
ac-ee <ace-md2024@outlook.com>
2024-06-21 01:46:39 +0300

    Gifted

--- config.cjs
+++ config.cjs
@@ -17,7 +17,7 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || 'public',
+  MODE: process.env.MODE || 'private,
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
