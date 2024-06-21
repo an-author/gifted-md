b6bcfa561a905bb3f7035e8a7dba58b1716fbbe1
ac-ee <ace-md2024@outlook.com>
2024-06-21 06:35:56 +0300

    Gifted

--- src/plugin/gdrive.js
+++ src/plugin/gdrive.js
@@ -20,7 +20,7 @@ const gdriveDownload = async (m, Matrix) => {
 
       if (gdriveInfo && gdriveInfo.status && gdriveInfo.data) {
         const mediaUrl = gdriveInfo.data;
-        const caption = `> © Powered By Ethix-MD`;
+        const caption = `> *©𝟐𝟎𝟐𝟒 𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃 𝐕𝟓*`;
 
         // Inferring the file type based on the file extension
         const extension = mediaUrl.split('.').pop().toLowerCase();
