792528ce434368c5ec8a4d0caf3e2214523be4fb
Gifted Tech <166598671+mouricedevs@users.noreply.github.com>
2024-06-24 09:03:06 +0300

    Gifted

--- gifted/cmds/alwaysonline.js
+++ gifted/cmds/alwaysonline.js
@@ -1,4 +1,4 @@
-import config from '../../config.cjs';
+import config from '../../set.cjs';
 
 const alwaysonlineCommand = async (m, Matrix) => {
   const botNumber = await Matrix.decodeJid(Matrix.user.id);
