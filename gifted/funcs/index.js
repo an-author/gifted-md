054b92edcb96e8ce4de338cee27867d44033966f
Gifted Tech <166598671+mouricedevs@users.noreply.github.com>
2024-06-24 07:47:30 +0300

    Gifted

--- gifted/funcs/index.js
+++ gifted/funcs/index.js
@@ -1,5 +1,5 @@
 import Handler from './gifted.js'
-import Callupdate from './anticall.js'
+import Callupdate from './giftech.js'
 import GroupUpdate from './gift.js'
 
 export { Handler, Callupdate, GroupUpdate };
