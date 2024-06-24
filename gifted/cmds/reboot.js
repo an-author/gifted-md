3c9ae69bbddb707d44376eb405bd4f668abde12e
Gifted Tech <166598671+mouricedevs@users.noreply.github.com>
2024-06-24 14:50:24 +0300

    Gifted

---
+++ gifted/cmds/reboot.js
@@ -0,0 +1,19 @@
+const restartBot = async (m) => {
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+
+  if (cmd === 'restart') {
+    try {
+    await m.reply('*Gifted-Md is Rebooting....*')
+      process.exit()
+    } catch (error) {
+      console.error(error);
+      await m.React("❌");
+      return m.reply(`An error occurred while restarting the bot: ${error.message}`);
+    }
+  }
+};
+
+export default restartBot;
+
