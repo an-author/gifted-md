556ab653bacdf17190dd4fcd8adc5b9261b43027
ac-ee <ace-md2024@outlook.com>
2024-06-27 06:21:22 +0300

    Gifted

--- gifted.js
+++ gifted.js
@@ -1,25 +1,2 @@
-const {
-   spawn
-} = require('child_process')
-const giftke = require('path')
-
-function startGifted() {
-   let giftek = [giftke.join(__dirname, './gifted/gifted'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...giftek].join('\n'))
-   let gift = spawn(process.argv[0], giftek, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Gifted...')
-            gift.kill()
-            startGifted()
-            delete gift
-         }
-      })
-      .on('exit', code => {
-         console.error('Process Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) startGifted()
-      })
-}
+const { startGifted } = require('./gifted/gifted')
 startGifted()
