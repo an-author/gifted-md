ea8e4142b649ed725391d0e6e5f27d6cf1c2b172
ac-ee <ace-md2024@outlook.com>
2024-06-28 16:54:30 +0300

    Gifted

--- gifted/cmds/alwaysonline.js
+++ gifted/cmds/alwaysonline.js
@@ -9,7 +9,7 @@ const alwaysonlineCommand = async (m, Matrix) => {
   const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
 
   if (cmd === 'alwaysonline') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*Owner Only Command!*");
     let responseMessage;
 
     if (text === 'on') {
