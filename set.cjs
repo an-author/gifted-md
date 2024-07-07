780f04e0f2646f5ef4e0a85e1963f6db9fe61490
ac-ee <ace-md2024@outlook.com>
2024-07-07 11:36:46 +0300

    Gifted

--- set.cjs
+++ set.cjs
@@ -26,7 +26,7 @@ const config = {
   PREFIX: process.env.PREFIX || '.',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254110853827",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
