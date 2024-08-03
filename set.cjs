5fde59fae71fc4576f8700d9eebdfd6eb25efe61
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-03 15:34:52 +0300

    Gifted

--- set.cjs
+++ set.cjs
@@ -22,7 +22,7 @@ const config = {
   
   AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.REJECT_CALL === 'false' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || 'private',
+  MODE: process.env.MODE || 'public',
   PREFIX: process.env.PREFIX || '.',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
   OWNER_NAME: process.env.OWNER_NAME || "GIFTED TECH",
